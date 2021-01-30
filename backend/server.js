8c829ba1527602cf99e5987ecb4663c2a38d207a
Senhe Hao <senhehao@protonmail.com>
2021-01-30 14:05:23 -0600

    opened the can of worms

--- backend/server.js
+++ backend/server.js
@@ -47,8 +47,6 @@ require("./passportConfig")(passport);
 
 // Routes
 
-require('./routes')(app)
-
 app.post("/login", (req, res, next) => {
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
@@ -62,7 +60,6 @@ app.post("/login", (req, res, next) => {
     }
   })(req, res, next);
 });
-/*
 app.post("/register", (req, res) => {
   User.findOne({ username: req.body.username }, async (err, doc) => {
     if (err) throw err;
@@ -79,11 +76,9 @@ app.post("/register", (req, res) => {
     }
   });
 });
-
 app.get("/user", (req, res) => {
   res.send(req.user); // The req.user stores the entire user that has been authenticated inside of it.
 });
-*/
 
 //----------------------------------------- END OF ROUTES---------------------------------------------------
 //Start Server
