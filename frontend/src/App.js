eaef16607b2a9c3ea3f65f8c573b941e77a04f30
Keegan <Keeg9087@gmail.com>
2021-01-30 17:35:28 -0600

    fix unmatched homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import Button from '@material-ui/core/Button';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import LoginPage from './LoginPage'
 import RegisterPage from "./RegisterPage";
-import Homepage from './Homepage'
+import ButtonAppBar from "./HomePage";
 
 
 function App() {
@@ -16,7 +16,7 @@ function App() {
   
   return (
     <Router>
-      <Homepage/>
+      <ButtonAppBar/>
         <Switch>
           <Route exact path="/login" component={LoginPage} />
           <Route exact path='/register' component={RegisterPage}/>
