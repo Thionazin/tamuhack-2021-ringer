7258b7370c4adbe0cec994baa6e923131758f6c9
GrickRimes <elierdman@yahoo.com>
2021-01-30 21:54:33 -0600

    enter

--- frontend/src/SelectGamesDropDown.js
+++ frontend/src/SelectGamesDropDown.js
@@ -12,7 +12,27 @@ import {setGameList} from './RegisterPage'
 const gameOptions = [
     { value: 'counter-strike', label: 'Counter Strike' },
     { value: 'destiny', label: 'Destiny' },
-    { value: 'destiny 2', label: 'Destiny 2' }
+    { value: 'destiny 2', label: 'Destiny 2' },
+    { value: 'rocket-league', label: 'Rocket League'},
+    { value: 'apex-legends', label: 'Apex Legends'},
+    { value: 'escape-from-tarkov', label: 'Escape From Tarkov'},
+    { value: 'dota 2', label: 'Dota 2'},
+    { value: 'fortnite', label: 'Fortnite'},
+    { value: 'rust', label: 'Rust'},
+    { value: 'league-of-legends', label: 'League of Legends'},
+    { value: 'overwatch', label: 'Overwatch'},
+    { value: 'pubg', label: 'PlayerUnknown\'s Battlegrounds'},
+    { value: 'starcraft 2', label: 'StarCraft II: Wings of Liberty'},
+    { value: 'super-smash-bros-ultimate', label: 'Super Smash Bros. Ultimate'},
+    { value: 'tekken 7', label: 'Tekken 7'},
+    { value: 'call-of-duty-warzone', label: 'Call of Duty Warzone'},
+    { value: 'valorant', label: 'Valorant'},
+    { value: 'rainbow-six-siege', label: 'Rainbow Six: Siege'},
+    { value: 'smite', label: 'Smite'},
+    { value: 'hearthstone', label: 'Hearthstone'},
+    { value: 'fifa', label: 'Fifa'},
+    { value: 'madden-nfl', label: 'Madden NFL'},
+    { value: 'nba-2k', label: 'NBA 2K'}
   ]
 
 const animatedComponents = makeAnimated();
